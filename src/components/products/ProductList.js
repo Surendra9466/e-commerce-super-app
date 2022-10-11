e2dccbff818b9c59b2d15c3ebb8f1cf27d678865
Surendra <ssingh@forbestech.com>
2022-10-11 23:08:51 +0530

    added loader and integrated api of product

--- src/components/products/ProductList.js
+++ src/components/products/ProductList.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ProductSearch from '../../assets/ProductSearch.svg'
 
-export default function ProductList() {
+export default function ProductList({product}) {
+    const {name, price, image} = product;
     return (
         <div className='product-item'>
             <div className='product-img-wrapper'>
-                <img className='product-img' src="https://dl.airtable.com/.attachmentThumbnails/89ba7458c24252be77f5a835dd398880/c13ef359" alt="img" />
+                <img className='product-img' src={image} alt={name} />
                 <div className='search-icon-wrapper cursor-pointer'><img className='search-icon' src={ProductSearch} /></div>
             </div>
             <div className='product-details flex justify-between mt-4 capitalize'>
-                <h5>modern poster</h5>
-                <p className='text-secondary'>$30.99</p>
+                <h5>{name}</h5>
+                <p className='text-secondary'>${price}</p>
             </div>
         </div>
     )
--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -1,50 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Breadcrumb from '../breadcrumb/Breadcrumb'
 import './product.scss';
 import ProductList from './ProductList';
+import Loader from '../loaderSpinner/Loader';
 // https://course-api.com/react-store-products
 export default function Products() {
+
+  const [productList, setProductList] = useState([]);
+  const [loader, setLoader] = useState(true);
+
+  useEffect(() => {
+    fetch('https://course-api.com/react-store-products')
+      .then((res) => res.json())
+      .then((data) => {
+        setProductList(data)
+        setLoader(false);
+      })
+
+  }, [])
 
   return (
     <>
       <Breadcrumb BreadcrumbValue={'Products'} />
       <main className='product-section lg:mt-8 font-poppins'>
-        <div className='product-container mx-auto lg:gap-12 flex-col md:flex-row'>
+        <div className='product-container mx-auto lg:gap-12 flex-col md:flex-row mb-12'>
           <aside className='filter-wrapper'>filter section</aside>
+
           <section className='product-item-wrapper flex-1'>
-            <div className='product-header'>
-              <div className='product-count text-base'>
-                23 products found
-              </div>
-            </div>
-            <div className='products-wrapper flex flex-wrap gap-6'>
-              <ProductList />
-              <ProductList />
-              <ProductList />
-              <ProductList />
-              <ProductList />
-              <ProductList />
-              <ProductList />
-              <ProductList />
-              <ProductList />
-              <ProductList />
-              <ProductList />
-              <ProductList />
-              <ProductList />
-              <ProductList />
-              <ProductList />
-              <ProductList />
-              <ProductList />
-              <ProductList />
-              <ProductList />
-              <ProductList />
-              <ProductList />
-              <ProductList />
-              <ProductList />
-              <ProductList />
-              <ProductList />
-              <ProductList />
-            </div>
+            {loader ? <Loader /> :
+              <>
+                <div className='product-header'>
+                  <div className='product-count text-base'>
+                    {productList.length} products found
+                  </div>
+                </div>
+                <div className='products-wrapper flex flex-wrap gap-6'>
+                  {productList.map((product, index) => {
+                    console.log(product);
+                    return <ProductList key={product.id} product={product} />
+                  })}
+                </div>
+              </>
+            }
           </section>
         </div>
       </main>
