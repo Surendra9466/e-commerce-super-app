5d242b630f5de2ae55ccb65ee24c738092b6dd14
forbes_marketplace <forbes-devs@media.net>
2022-11-10 10:07:12 +0530

    select color functionality

--- src/components/singleProduct/SingleProductPage.js
+++ src/components/singleProduct/SingleProductPage.js
@@ -3,11 +3,11 @@ import './single-product.scss';
 import { Link, useParams } from 'react-router-dom';
 import Breadcrumb from '../breadcrumb/Breadcrumb';
 import Slider from '../slider/Slider.js';
+import Button from './Button';
 
 export default function SingleProductPage() {
   const [singleProduct, setSingleProduct] = useState({});
   const { productId } = useParams();
-
   const breadcrumbData = [
     {
       name: 'Home',
@@ -18,7 +18,6 @@ export default function SingleProductPage() {
       path: '/products'
     }
   ]
-  console.log(singleProduct.images);
   useEffect(() => {
     fetch(`https://course-api.com/react-store-single-product?id=${productId}`)
       .then((res) => res.json())
@@ -31,6 +30,34 @@ export default function SingleProductPage() {
         <Link className="back-to-product uppercase cursor-pointer" to="/products">back to products</Link>
         <div className='single-product-section'>
           {singleProduct.images && <Slider sliderImages={singleProduct.images} />}
+          <section className='content'>
+            <h2 className='title capitalize font-bold'>{singleProduct.name}</h2>
+            <div>{singleProduct.stars}</div>
+            <h5 className='price text-secondary font-bold'>${singleProduct.price}</h5>
+            <p className='desc'>{singleProduct.description}</p>
+            <p className='info'>
+              <span className='font-bold'>Available:</span>
+              <span>{singleProduct.stock > 0 ? 'In stock' : 'Out of stock'}</span>
+            </p>
+            <p className='info'>
+              <span className='font-bold'>SKU :</span>
+              <span>{singleProduct.id}</span>
+            </p>
+            <p className='info'>
+              <span className='font-bold'>Brand</span>
+              <span>{singleProduct.company}</span>
+            </p>
+          </section>
+          <section className='add-to-cart'>
+            <div className='colors'>
+              <span className='capitalize font-bold'>colors :</span>
+              <div className='flex'>
+                {singleProduct.colors?.map((color, index) => {
+                  return <Button key={index} color={color} index={index}/>
+                })}
+              </div>
+            </div>
+          </section>
         </div>
       </div>
     </div>
