8409cb459816000c75a3af99eba3cc92bf66a4c0
forbes_marketplace <forbes-devs@media.net>
2022-11-10 12:03:40 +0530

    Created about us page

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,11 +1,30 @@
 import React from 'react'
 import Breadcrumb from '../breadcrumb/Breadcrumb'
+import aboutUs from '../../assets/aboutUs.jpeg';
+import './about_us.scss';
 
 export default function About() {
+
+  const breadcrumbData = [
+    {
+      name: 'Home',
+      path: '/'
+    }
+  ]
+
   return (
     <>
-      {/* <Breadcrumb BreadcrumbValue={'About'} /> */}
-      <div className='about_us'>About Components</div>
+      <Breadcrumb BreadcrumbValue="About" breadcrumbData={breadcrumbData} />
+      <div className='about_us'>
+        <img className='about-us-img' src={aboutUs} alt="about-us-img" />
+        <article>
+          <h2 className='title'>Our Story</h2>
+          <div className='underline'></div>
+          <p className='content'>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat accusantium sapiente tempora sed dolore esse deserunt eaque excepturi, delectus error accusamus vel eligendi, omnis beatae. Quisquam, dicta. Eos quod quisquam esse recusandae vitae neque dolore, obcaecati incidunt sequi blanditiis est exercitationem molestiae delectus saepe odio eligendi modi porro eaque in libero minus unde sapiente consectetur architecto. Ullam rerum, nemo iste ex, eaque perspiciatis nisi, eum totam velit saepe sed quos similique amet. Ex, voluptate accusamus nesciunt totam vitae esse iste.
+          </p>
+        </article>
+      </div>
     </>
   )
 }
--- src/components/breadcrumb/Breadcrumb.js
+++ src/components/breadcrumb/Breadcrumb.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import './breadcrum.scss'
 import { Link } from 'react-router-dom';
-export default function Breadcrumb({ BreadcrumbValue, breadcrumbData }) {
+export default function Breadcrumb({ BreadcrumbValue="", breadcrumbData }) {
+  console.log(breadcrumbData)
   return (
     <div className='breadcrumb-wrapper bg-accent flex items-center px-4'>
-      {BreadcrumbValue ? <div className='breadcrumb text-secondary text-xl font-semibold mx-auto '>
+      {breadcrumbData.length > 0 ? <div className='breadcrumb text-secondary text-xl font-semibold mx-auto '>
         {breadcrumbData.map((item, index) => {
           return <Link key={index} to={item.path}>{item.name} / </Link>
         })}
-        <span className='capitalize'>{BreadcrumbValue}</span>
+        {BreadcrumbValue ? <span className='capitalize'>{BreadcrumbValue}</span> : null}
       </div> : ''}
     </div>
   )
